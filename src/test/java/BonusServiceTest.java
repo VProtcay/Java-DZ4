b94e3c1e56bfc3e5a0b39409527b08f2c893cc62
Vladimir Protcay <protcay2@mail.ru>
2021-12-29 03:36:16 +0300

    добавил четвертый тест

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -37,12 +37,30 @@ class BonusServiceTest {
 
     }
     @Test
-    void shouldCalculateNoRegistered() {
+    void shouldCalculateNoRegisteredAndBonusOverLimit() {
         BonusService service = new BonusService();
         // подготавливаем данные
         long amount = 1000_60;
         boolean registered = false;
         long expected = 10;
+
+
+        // вызываем целевой метод
+        long actual = service.calculate(amount, registered);
+
+
+        // выводим результат
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNoRegisteredAndBonusUnderLimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные
+        long amount = 100_60;
+        boolean registered = false;
+        long expected = 1;
+
 
         // вызываем целевой метод
         long actual = service.calculate(amount, registered);
